a462fd2091bbc1e4afc60f025bd9fcea128872ed
Sergio Valencia <sergio.ruben.vm@gmail.com>
2025-12-08 11:20:55 -0500

    feat: se agrega el manejo de excepciones personalizadas para módulo de usuarios

---
+++ src/main/java/com/example/demo/exception/UsuarioNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class UsuarioNoEncontradoException extends RuntimeException {
+    public UsuarioNoEncontradoException(String message) {
+        super(message);
+    }
+}
+
